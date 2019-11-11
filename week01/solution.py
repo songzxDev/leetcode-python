c9d3473e0a211785599b44a23f6ea80e2a802f57
songzxDev <songzx_2326@163.com>
2019-11-11 17:08:34 +0800

    """ 题号：283.移动零 标签：数组双指针 :type nums: List[int] :rtype: None Do not return anything, modify nums in-place instead. """

--- week01/solution.py
+++ week01/solution.py
@@ -187,4 +187,31 @@ class Solution(object):
                 n -= 1
         if n > 0:
             nums1[0:n] = nums2[0:n]
+
+    # 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+    #
+    # 示例:
+    #
+    # 输入: [0,1,0,3,12]
+    # 输出: [1,3,12,0,0]
+    #
+    # 说明:
+    #
+    #
+    # 必须在原数组上操作，不能拷贝额外的数组。
+    # 尽量减少操作次数。
+    #
+    # Related Topics 数组 双指针
+    def moveZeroes(self, nums):
+        """
+        题号：283.移动零
+        标签：数组双指针
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        zero = 0
+        for i in xrange(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[zero] = nums[zero], nums[i]
+                zero += 1
 # leetcode submit region end(Prohibit modification and deletion)
