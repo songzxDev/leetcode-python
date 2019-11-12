358e30c94bd25732f4d680f3386eefb5630bed0e
songzxDev <songzx_2326@163.com>
2019-11-12 13:47:21 +0800

    """ 题目：70.爬楼梯 标签：数组 数学 动态规划 :type n: int :rtype: int """

--- week01/solution.py
+++ week01/solution.py
@@ -241,4 +241,33 @@ class Solution(object):
                 j -= 1
             ans = max(ans, (j - i + 1) * minhgt)
         return ans
+
+    # 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+    # 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+    # 注意：给定 n 是一个正整数。
+    # 示例 1：
+    # 输入： 2
+    # 输出： 2
+    # 解释： 有两种方法可以爬到楼顶。
+    # 1.  1 阶 + 1 阶
+    # 2.  2 阶
+    # 示例 2：
+    # 输入： 3
+    # 输出： 3
+    # 解释： 有三种方法可以爬到楼顶。
+    # 1.  1 阶 + 1 阶 + 1 阶
+    # 2.  1 阶 + 2 阶
+    # 3.  2 阶 + 1 阶
+    # Related Topics 动态规划
+    def climbStairs(self, n):
+        """
+        题目：70.爬楼梯
+        标签：数组 数学 动态规划
+        :type n: int
+        :rtype: int
+        """
+        i, tmp = 3, {1: 1, 2: 2}
+        while i <= n:
+            tmp[i], i = tmp[i - 1] + tmp[i - 2], i + 1
+        return tmp[n]
 # leetcode submit region end(Prohibit modification and deletion)
