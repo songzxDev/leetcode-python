990d8d9f12f43a516ad8220fb1eeee87b4c7c4e4
songzxDev <songzx_2326@163.com>
2019-11-15 12:25:39 +0800

    """         题目：125.验证回文串         标签：双指针 字符串         备注：不使用python的replace函数，速度很快         :type s: str         :rtype: bool         """

--- week01/solution.py
+++ week01/solution.py
@@ -419,4 +419,35 @@ class Solution(object):
                 snowball += 1
             elif snowball > 0:
                 nums[i], nums[i - snowball] = nums[i - snowball], nums[i]
+
+    # 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+    # 说明：本题中，我们将空字符串定义为有效的回文串。
+    # 示例 1:
+    # 输入: "A man, a plan, a canal: Panama"
+    # 输出: true
+    # 示例 2:
+    # 输入: "race a car"
+    # 输出: false
+    # Related Topics 双指针 字符串
+    def isPalindrome(self, s):
+        """
+        题目：125.验证回文串
+        标签：双指针 字符串
+        备注：不使用python的replace函数，速度很快
+        :type s: str
+        :rtype: bool
+        """
+        if s is None:
+            return False
+        start, end = 0, len(s) - 1
+        while start <= end:
+            while start <= end and not s[start].isalnum():
+                start += 1
+            while start <= end and not s[end].isalnum():
+                end -= 1
+            if start <= end and s[start].lower() != s[end].lower():
+                return False
+            start += 1
+            end -= 1
+        return True
 # leetcode submit region end(Prohibit modification and deletion)
