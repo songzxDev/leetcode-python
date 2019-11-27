8a9cae3b00006d3ed5662958266c7d2416adb84e
songzxDev <songzx_2326@163.com>
2019-11-27 11:09:11 +0800

    """ 题目：977.有序数组的平方 标签：数组 双指针 :type A: List[int] :rtype: List[int] """

--- week02/solution02.py
+++ week02/solution02.py
@@ -33,3 +33,35 @@ class Solution(object):
                 stack.append(stack[-1] + stack[-2] if len(stack) > 1 else stack[-1])
                 sums += stack[-1]
         return sums
+
+    # 给定一个按非递减顺序排序的整数数组 A，返回每个数字的平方组成的新数组，要求也按非递减顺序排序。
+    # 示例 1：
+    # 输入：[-4,-1,0,3,10]
+    # 输出：[0,1,9,16,100]
+    # 示例 2：
+    # 输入：[-7,-3,2,3,11]
+    # 输出：[4,9,9,49,121]
+    # 提示：
+    # 1 <= A.length <= 10000
+    # -10000 <= A[i] <= 10000
+    # A 已按非递减顺序排序。
+    # Related Topics 数组 双指针
+    def sortedSquares(self, A):
+        """
+        题目：977.有序数组的平方
+        标签：数组 双指针
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        if A is None or len(A) == 0:
+            return []
+        if A[0] >= 0:
+            return map(lambda x: x * x, A)
+        i, j, p, res = 0, len(A) - 1, len(A) - 1, [0] * len(A)
+        while p >= 0:
+            if abs(A[i]) > abs(A[j]):
+                res[p], i = A[i] * A[i], i + 1
+            else:
+                res[p], j = A[j] * A[j], j - 1
+            p -= 1
+        return res
