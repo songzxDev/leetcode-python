04ddbf8e0b7586ca8eec216289a6864eaed6ab4a
songzxDev <songzx_2326@163.com>
2020-02-16 10:17:01 +0800

    387题

---
+++ homework/week08/LeetCode_387_1034.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/2/16 0016 9:48
+# @Author  : songzhenxi
+# @Email   : songzx_2326@163.com
+# @File    : LeetCode_387_1034.py
+# @Software: PyCharm
---
+++ homework/week08/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/2/16 0016 9:47
+# @Author  : songzhenxi
+# @Email   : songzx_2326@163.com
+# @File    : __init__.py.py
+# @Software: PyCharm
